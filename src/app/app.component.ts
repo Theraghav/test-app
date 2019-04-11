389af92ba66d0c93d5d13779fc3eb8a169776c1c
Lab@IECL <lab@ifelsecloud.com>
2019-04-12 10:15:57 +1200

    Baraa- pending task.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 
 // @ts-ignore
 @Component({
@@ -6,9 +6,27 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+@ViewChild('test') testRef: ElementRef;
   title = 'test-app';
   startDate: string;
   currentDate: string;
   endDateCustom: string;
+
+  constructor(){}
+
+  ngOnInit():void{
+
+  }
+
+  print(...elms):void{
+    const [leftSec, rightSecTop, rightSecBottom] = [...elms];
+    const leftSecText = (<HTMLDivElement>leftSec).innerText;
+    const rightSecTopHtml = (<HTMLDivElement>rightSecTop).outerHTML;
+    const rightSecBottomHtml = (<HTMLDivElement>rightSecBottom).outerHTML;
+    (<HTMLDivElement>this.testRef.nativeElement)
+    .innerHTML = `${leftSecText} 
+    <br> ${rightSecTopHtml} 
+    <br> ${rightSecBottomHtml}`;
+  }
 }
