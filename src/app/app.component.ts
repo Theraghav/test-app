96362caf26e88d657f17c85453913b3c3183b22a
Lab@IECL <lab@ifelsecloud.com>
2019-04-12 15:38:43 +1200

    barah uncommited work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material';
 
 // @ts-ignore
 @Component({
@@ -8,18 +9,20 @@ import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 @ViewChild('test') testRef: ElementRef;
+@ViewChild('printModal') printModalRef: ElementRef;
+
   title = 'test-app';
   startDate: string;
   currentDate: string;
   endDateCustom: string;
 
-  constructor(){}
+  constructor(public printModal: MatDialog){}
 
   ngOnInit():void{
 
   }
 
-  print(...elms):void{
+  openPrintModal(...elms):void{
     const [leftSec, rightSecTop, rightSecBottom] = [...elms];
     const leftSecText = (<HTMLDivElement>leftSec).innerText;
     const rightSecTopHtml = (<HTMLDivElement>rightSecTop).outerHTML;
@@ -28,5 +31,14 @@ export class AppComponent implements OnInit {
     .innerHTML = `${leftSecText} 
     <br> ${rightSecTopHtml} 
     <br> ${rightSecBottomHtml}`;
+
+    const modal =  this.printModal.open(PrintModalComp, {
+      width: '90%',
+      data: {leftSec, rightSecTopHtml, rightSecBottomHtml}
+    });
   }
+}
+
+class PrintModalComp {
+  constructor(private modalRef: MatDialogRef<PrintModalComp)
 }
